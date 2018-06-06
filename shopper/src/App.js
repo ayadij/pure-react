5b59063cd4a1843d77d1db75d1ae3ad3d527e1af
ayadij <ayadijkwel@yahoo.com>
2018-06-06 17:28:26 -0600

    finished shopping cart app

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './Nav';
 import ItemPage from './ItemPage'; 
+import CartPage from './CartPage';
 import {items} from './static-data';
 import './App.css';
 
@@ -9,7 +10,8 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    activeTab: 0
+    activeTab: 0,
+    cart: []
   };
 
   handleTabChange = (index) => {
@@ -17,19 +19,63 @@ class App extends React.Component {
       activeTab: index
     });
   }
-  // ..handleAddToCart
-  // ..handleRemoveOne
+  
+  handleAddToCart = (item) => {     // sets the cart state to a copy of the current cart, plus one new item
+    this.setState({
+      cart: [...this.state.cart, item.id] //spread operator - expands the array into individual items
+    });
+  }
+
+  handleRemoveOne = (item) => {
+    let index = this.state.cart.indexOf(item.id);
+    this.setState({
+      cart: [
+        ...this.state.cart.slice(0, index),
+        ...this.state.cart.slice(index + 1)
+      ]
+    });
+  }
 
 
   renderContent() {
     switch(this.state.activeTab) {
       default:
-        case 0: return <ItemPage items={items} />
-        case 1: return <span>Cart placeholder</span>
+        case 0: 
+          return (
+          <ItemPage 
+            items={items} 
+            onAddToCart={this.handleAddToCart}/>
+          );
+        case 1: 
+          return this.renderCart();
     }
   }
 
-  // .. renderCart()
+  renderCart() {
+    let itemCounts = this.state.cart.reduce((itemCounts, itemId) => { 
+      itemCounts[itemId] = itemCounts[itemId] || 0; 
+      itemCounts[itemId]++;
+      return itemCounts;
+    }, {});
+    
+    let cartItems = Object.keys(itemCounts).map(itemId => { // Find the item by its id
+      var item = items.find(item =>
+        item.id === parseInt(itemId, 10) 
+      );
+            
+      return {       // Create a new "item" and add the 'count' property
+        ...item,
+        count: itemCounts[itemId] 
+      }
+    });
+    return (
+      <CartPage 
+        items={cartItems} 
+        onAddOne={this.handleAddToCart}
+				onRemoveOne={this.handleRemoveOne} />
+    ); 
+  }
+
 
   render() {
     let {activeTab} = this.state;
--- shopper/src/ItemPage.js
+++ shopper/src/ItemPage.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Item from './Item';
 import './ItemPage.css';
 
-function ItemPage({ items }) {
+function ItemPage({ items, onAddToCart }) {
     return (
         <ul className="ItemPage-items">
             {items.map(item =>          // iterating over a list of items and redering them out
                 <li key={item.id} className="ItemPage-item">
-                    {item.name}
+                    <Item item={item}>
+                        <button className="Item-addToCart" onClick={() => onAddToCart(item)} >
+                        Add to Cart
+                        </button>
+                    </Item>
                 </li>
             )}
         </ul>
     );
 }
 ItemPage.propTypes = {
-    items: PropTypes.array.isRequired
+    items: PropTypes.array.isRequired,
+    onAddToCart: PropTypes.func.isRequired
 };
 
 export default ItemPage;
